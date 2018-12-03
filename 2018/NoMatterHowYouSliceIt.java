00b6e3806677d927d6c528445eaba14fd9e87120
Loic Beylot <loic.beylot@gmail.com>
2018-12-03 06:57:05 +0000

    Day 3 first code

---
+++ 2018/NoMatterHowYouSliceIt.java
@@ -0,0 +1,11 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class NoMatterHowYouSliceIt {
+    private static final Pattern p = Pattern.compile("^#[0-9]+ @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)");
+    
+    
+    public static void main(String[] args) {
+    }
+}
